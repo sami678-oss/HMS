f9bd191ae99428f777af0f96702bd3bf8a012986
sami678-0ss <adnansamiq2003@gmail.com>
2025-01-10 19:30:43 +0530

    first commit

--- frontend/src/pages/CheckIn.jsx
+++ frontend/src/pages/CheckIn.jsx
@@ -1,7 +1,14 @@
 import React from "react";
 
 const Checkin = () => {
-  return <div>Checkin</div>;
+  return <div>Checkin
+    <form action="" className="flex flex-col h-[500px] w-[400px] border-[1px] border-black">
+      <h1 >LOGIN</h1>
+      <input type="text" name="text" placeholder="Enter your id" />
+      <input type="submit" value="Confirm"/>
+    </form>
+  </div>;
 };
 
 export default Checkin;
+
