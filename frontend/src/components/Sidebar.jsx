cf6a86baa813f0a1ae186556056609befe9d4b88
Pavithra Kella <159472251+pavithrakella@users.noreply.github.com>
2025-01-10 20:46:36 +0530

    Update Sidebar.jsx now

--- frontend/src/components/Sidebar.jsx
+++ frontend/src/components/Sidebar.jsx
@@ -1,67 +1,64 @@
 
-import React, { useState } from 'react'
+import React from 'react';
 import {
     FaTh,
     FaBars,
     FaUserAlt,
-    FaRegChartBar,
-    FaCommentAlt,
-    FaShoppingBag,
-    FaThList,
-    FaRegClock 
-}from "react-icons/fa";
+    FaRegClock
+} from "react-icons/fa";
 import { NavLink } from 'react-router-dom';
 
-const Sidebar = ({children}) => {
-  
-    const menuItem=[
-                {
-                    path:"/",
-                    name:"Dashboard",
-                    icon:<FaTh/>
-                },
-                {
-                    path:"/userdetails",
-                    name:"UserDetails",
-                    icon:<FaUserAlt/>
-                },
-              
-                {
-                    path:"/checkin",
-                    name:"CheckIn",
-                    icon:< FaRegClock />
-                },
-                {
-                    path:"/checkout",
-                    name:"CheckOut",
-                    icon:< FaRegClock />
-                },
-               
-            ]
+const Sidebar = ({ children }) => {
 
-            return(
-                <div className="container">
-                    <div   className="sidebar">
-                        <div className="top_section">
-                                <h1 className='logo'>Logo</h1>
-                            <div  className='bars'>
-                                    <FaBars />
-                            </div>
-                                    
-                            </div>
-                            {
-                            menuItem.map((item, index)=>(
-                                <NavLink to={item.path} key={index} className="link" activeclassName="active">
-                                    <div className="icon">{item.icon}</div>
-                                    <div  className="link_text">{item.name}</div>
-                                </NavLink>
-                             ))
-                             }
+    const menuItem = [
+        {
+            path: "/",
+            name: "Dashboard",
+            icon: <FaTh />
+        },
+        {
+            path: "/userdetails",
+            name: "UserDetails",
+            icon: <FaUserAlt />
+        },
+        {
+            path: "/checkin",
+            name: "CheckIn",
+            icon: <FaRegClock />
+        },
+        {
+            path: "/checkout",
+            name: "CheckOut",
+            icon: <FaRegClock />
+        },
+    ];
 
-                    </div>    
-                    <main>{children}</main>
+    return (
+        <div className="container">
+            <div className="sidebar">
+                <div className="top_section">
+                    <div className="header">
+                        <div className="logo-container">
+                            <div className="logo-icon">HM</div>
+                            <h1 className="logo-text">Hospitality Management</h1>
+                        </div>
+                        <div className="bars">
+                            <FaBars />
+                        </div>
+                    </div>
                 </div>
-            )
+                {
+                    menuItem.map((item, index) => (
+                        <NavLink to={item.path} key={index} className="link" activeClassName="active">
+                            <div className="icon">{item.icon}</div>
+                            <div className="link_text">{item.name}</div>
+                        </NavLink>
+                    ))
+                }
+            </div>
+            <main>{children}</main>
+        </div>
+    );
 }
 
-export default Sidebar
+export default Sidebar;
