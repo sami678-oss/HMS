8e91fba426a84dc2bcc7112dc4ae3a8cffca0539
Pavithra Kella <159472251+pavithrakella@users.noreply.github.com>
2025-01-20 09:01:23 +0530

    frontend

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -1,49 +1,4 @@
-<<<<<<< HEAD
 
-
-
-import React, { useEffect, useState } from "react";
-
-const Dashboard = ({ stats, userId }) => {
-
-  // Fetch user details by tzkid
-  useEffect(() => {
-    const fetchDashboardStats = async () => {
-      try {
-        const response = await fetch("http://localhost:4001/api/stats");
-        const data = await response.json();
-  
-        if (response.ok) {
-          setDashboardStats(data.stats);
-        } else {
-          console.error("Error fetching stats:", data.message);
-        }
-      } catch (error) {
-        console.error("Error fetching stats:", error);
-      }
-    };
-  
-    fetchDashboardStats();
-  }, []);
-  
-
-
-
-  return (
-    <div>
-      <h1>Dashboard</h1>
-      <h2>Boys Hostel (I3)</h2>
-      <p>Total Arrivals: {stats.boysHostel.totalArrivals}</p>
-      <p>Rooms Occupied: {stats.boysHostel.roomsOccupied}</p>
-      <p>Available Rooms: {stats.boysHostel.availableRooms}</p>
-
-      <h2>Girls Hostel (K4)</h2>
-      <p>Total Arrivals: {stats.girlsHostel.totalArrivals}</p>
-      <p>Rooms Occupied: {stats.girlsHostel.roomsOccupied}</p>
-      <p>Available Rooms: {stats.girlsHostel.availableRooms}</p>
-
-     
-=======
 import React, { useState } from "react";
 import "./Dashboard.css";
 
@@ -129,7 +84,7 @@ const Dashboard = ({ stats }) => {
           </tbody>
         </table>
       </div>
->>>>>>> a3a666da2085c23961710fccf59ba003ed6a1224
+
     </div>
   );
 };
